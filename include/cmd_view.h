c7846c5eaa29af0bfd0b98c27e47d9a30dd1debf
Kimo16 <karimmezher@hotmail.fr>
2019-03-08 14:41:57 +0100

    Texture

---
+++ include/cmd_view.h
@@ -0,0 +1,4 @@
+#ifndef CMD_VIEW_H
+#define CMD_VIEW_H
+
+#endif
--- include/m_frame.h
+++ include/m_frame.h
@@ -1,9 +1,9 @@
 #ifndef M_FRAME_H
 #define M_FRAME_H
 	
-	short truncate_image(SDL_Surface * img , int x1 , int y1 , int x2 ,int y2);
-	short resize_workspace(SDL_Surface* img,int witdh , int height );
-	short resize_image(SDL_Surface* img, int width, int height);
+	short truncate_image(SDL_Texture * img , int x1 , int y1 , int x2 ,int y2);
+	short resize_workspace(SDL_Texture* img,int witdh , int height );
+	short resize_image(SDL_Texture* img, int width, int height);
 
 
 #endif 

ef73685506a72584db20da651ad05dccc17f494b
leethater <budzkoal@gmail.com>
2019-03-21 21:54:33 +0100

    free_image passed to void

--- include/m_image.h
+++ include/m_image.h
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct image image;
 image *new_img(char *path);
@@ -15,6 +16,6 @@ short set_img_name(image *img, char *name);
 short set_img_path(image *img, char *path);
 short set_img_ext(image *img, char *ext);
 short set_img_surface(image *img, SDL_Surface *texture);
-short free_image(image *image);
+void free_image(image *image);
 
 #endif

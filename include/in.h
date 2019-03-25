76eee12217cd5a06c389273a3eb698212e3183fa
Kolibs <etienne@marais.green>
2019-03-25 14:13:45 +0100

    Manage Input file

--- include/in.h
+++ include/in.h
@@ -4,9 +4,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+#include <unistd.h>
+#include <dirent.h>
 #include "m_image.h"
 
 image* load_image(char* path); /*load an image*/
-short check_tmp(char* current_dir);
+short check_tmp();
 
 #endif
---
+++ src/model/in.c
@@ -0,0 +1,56 @@
+ #include "in.h"
+
+/**
+ * Permet de charger une image spécifiée par 
+ * [path]
+ */
+image *load_image(char *path) {
+  image *img = new_img(path);
+  if (img == NULL) {
+    fprintf(stderr, "DEBUG : wrong image path.\n");
+    return NULL;
+  }
+  SDL_Surface *tmp = IMG_Load(path);
+  if (tmp == NULL) {
+    fprintf(stderr, "DEBUG : can't load image.\n");
+    free_image(img);
+    return NULL;
+  }
+  SDL_PixelFormat *format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
+  if (format == NULL) {
+    fprintf(stderr, "DEBUG : can't allocate format.\n");
+    SDL_FreeSurface(tmp);
+    free_image(img);
+    return NULL;
+  }
+  SDL_Surface *copy = SDL_ConvertSurface(tmp, format, 0);
+  SDL_FreeSurface(tmp);
+  if (copy == NULL) {
+    fprintf(stderr, "DEBUG : can't convert image.\n");
+    SDL_FreeFormat(format);
+    return NULL;   
+  }
+  set_img_surface(img, copy);
+  SDL_FreeFormat(format);
+  return img;
+}
+
+short check_tmp() {
+  char * tmp_dir =  "/tmp/cimpletmp/";
+  DIR *dir = opendir(tmp_dir);
+  int ret = 0;
+  if (dir == NULL) {
+    fprintf(stderr, "No file saved");
+    return 0;
+  }
+  struct dirent *current;
+  while((current = readdir(dir)) != NULL) {
+    if(memcmp(current->d_name, "./", 2) == 0 ||
+       memcmp(current->d_name, "../", 3) == 0) {
+      continue;
+    }
+    printf("%s%s",tmp_dir,current->d_name);
+  }
+  closedir(dir);
+  return ret;
+}

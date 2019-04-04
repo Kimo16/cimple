9f2c7d4de2c4804bdea100269b2b3a25ca02af66
leethater <budzkoal@gmail.com>
2019-04-04 13:58:01 +0200

    rotate

--- src/model/m_transform.c
+++ src/model/m_transform.c
@@ -1,6 +1,7 @@
 #include "m_transform.h"
 
 /**
+ * 
  * Colors the (x,y) pixel with the specific color
  *
  * @param surface target surface
@@ -86,7 +87,7 @@ short symmetry(SDL_Surface *img, short vertical){
 }
 
 /**
- * Rotates the image
+ * Rotates the image clockwise
  *
  * @param img target surface, gets replaced by a new one
  * @param angle a positive miltiple of 90,the angle to rotate the surface
@@ -94,5 +95,90 @@ short symmetry(SDL_Surface *img, short vertical){
  */
 
 short rotate(SDL_Surface *img, int angle){
+	if(img==NULL){
+    fprintf(stderr, "Null image in rotate");
+    return 0;
+  }
+  if(angle%90!=0 || angle < 0){
+    fprintf(stderr, "Invalid angle in rotate");
+    return 0;
+  }
+	SDL_Surface *new_surface;
+	if(angle%90!=0){
+		fprintf(stderr, "Angle is not mod 90");
+		return 0;
+	}
+	if((angle/90)%4==0){
+		return 1;
+	}
+	if((angle/90)%4==1){
+		new_surface=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
+		if(new_surface == NULL){
+			SDL_Log("SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+			exit(1);
+		}
+		SDL_LockSurface(new_surface);
+		SDL_LockSurface(img);
+		Uint32 *pixels_ref=img->pixels;
+		int     width=new_surface->w;
+		for(int i=0; i < img->h; i++)
+			for(int j=0; j < img->w; j++){
+				SDL_Color c_ref={0};
+				SDL_GetRGBA(pixels_ref[i * img->w + j], new_surface->format, &c_ref.r, &c_ref.g, &c_ref.b, &c_ref.a);
+				int rc=setPixel(new_surface, c_ref, j, width - i);
+				if(rc == 0){
+					fprintf(stderr, "pixel not set");
+					return 0;
+				}
+			}
+	}
+	if((angle/90)%4==2){
+		new_surface=SDL_CreateRGBSurfaceWithFormat(0, img->w, img->h, 32, img->format->format);
+		if(new_surface == NULL){
+			SDL_Log("SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+			exit(1);
+		}
+		SDL_LockSurface(new_surface);
+		SDL_LockSurface(img);
+		Uint32 *pixels_ref=img->pixels;
+		int     height=new_surface->h;
+		int     width=new_surface->w;
+		for(int i=0; i < img->h; i++)
+			for(int j=0; j < img->w; j++){
+				SDL_Color c_ref={0};
+				SDL_GetRGBA(pixels_ref[i * img->w + j], new_surface->format, &c_ref.r, &c_ref.g, &c_ref.b, &c_ref.a);
+				int rc=setPixel(new_surface, c_ref, height - i, width - j);
+				if(rc == 0){
+					fprintf(stderr, "pixel not set");
+					return 0;
+				}
+			}
+	}
+	if((angle/90)%4==3){
+		new_surface=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
+		if(new_surface == NULL){
+			SDL_Log("SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+			exit(1);
+		}
+		SDL_LockSurface(new_surface);
+		SDL_LockSurface(img);
+		Uint32 *pixels_ref=img->pixels;
+		int     height=new_surface->h;
+		for(int i=0; i < img->h; i++)
+			for(int j=0; j < img->w; j++){
+				SDL_Color c_ref={0};
+				SDL_GetRGBA(pixels_ref[i * img->w + j], new_surface->format, &c_ref.r, &c_ref.g, &c_ref.b, &c_ref.a);
+				int rc=setPixel(new_surface, c_ref, height-j, i);
+				if(rc == 0){
+					fprintf(stderr, "pixel not set");
+					return 0;
+				}
+			}
+	}
+	SDL_UnlockSurface(new_surface);
+	SDL_UnlockSurface(img);
+	SDL_Surface *tmp=img;
+	img=new_surface;
+	SDL_FreeSurface(tmp);
 	return 1;
 }

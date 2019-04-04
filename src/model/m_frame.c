4d8afd13de9146e5664fff46885f5a8e223922b6
leethater <budzkoal@gmail.com>
2019-04-04 14:04:38 +0200

    m_frame prototype

---
+++ src/model/m_frame.c
@@ -0,0 +1,13 @@
+#include "m_frame.h"
+
+short truncate_image(SDL_Surface * img , int x1 , int y1 , int x2 ,int y2){
+    return 0;
+}
+
+short resize_workspace(SDL_Surface* img,int witdh , int height ){
+    return 0;
+}
+
+short resize_image(SDL_Surface* img, int width, int height){
+    return 0;
+}

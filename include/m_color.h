7c6827810326ebc663e33756c416b3b33a504d86
Kimo16 <karimmezher@hotmail.fr>
2019-04-06 21:51:50 +0200

    some review corrections

--- include/m_color.h
+++ include/m_color.h
@@ -3,10 +3,10 @@
 
 #include <SDL2/SDL.h>
 
-short negative_filter(SDL_Surface* img);
-short black_and_white_filter(SDL_Surface* img);
-short grey_filter(SDL_Surface* img,int level);
-short replace_color(SDL_Surface* img,SDL_Color origin_color,SDL_Color target_color,int margin);
-short color_zone(SDL_Surface* img ,SDL_Color color,int x1,int x2,int y1,int y2);
+short negative_filter(image * img);
+short black_and_white_filter(image * img);
+short grey_filter(image * img);
+short replace_color(image * img,SDL_Color origin_color,SDL_Color target_color,int margin);
+short color_zone(image * img ,SDL_Color color,int x1,int x2,int y1,int y2);
 
 #endif
--- src/model/m_color.c
+++ src/model/m_color.c
@@ -1,43 +1,45 @@
 #include "m_color.h"
+
 #include <math.h>
 
 
 /**
  * Change all surface pixels with negative filter
  *
- * @param SDL_surface * img , pointer to a surface representing an image
+ * @param image * img , pointer to an image structure representing an image
  * @return 0 if changes failed , 1 if all changes done.
  */
 
-short negative_filter(SDL_Surface *img){
+short negative_filter(image * img){
 	if(img == NULL){
 		fprintf(stderr, "Error negative_filter(SDL_Surface * img) : Null argument \n");
 		return 0;
 	}
-	SDL_Surface *neg_img=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
-	if(neg_img == NULL){
-			fprintf(stderr,"SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
-			return 0;
+	SDL_Surface * surface = img -> img;
+	SDL_Surface *neg_surface=SDL_CreateRGBSurfaceWithFormat(0, surface ->h, surface->w, 32, surface->format->format);
+	if(neg_surface == NULL){
+		fprintf(stderr, "SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+		return 0;
 	}
 
-	if(SDL_MUSTLOCK(neg_img) == 1) SDL_LockSurface(neg_img);
-	if(SDL_MUSTLOCK(img) == 1) SDL_LockSurface(img);
+	if(SDL_MUSTLOCK(neg_surface) == 1) SDL_LockSurface(neg_surface);
+	if(SDL_MUSTLOCK(surface) == 1) SDL_LockSurface(surface);
 
-	Uint32 *pixel_tmp = img->pixels;
-	Uint32 *pixel_neg_tmp = neg_img->pixels;
-
-	for(int i=0; i < neg_img ->h; i++)
-		for(int j=0; j < neg_img->w; j++){
+	Uint32 * src_pixels = surface -> pixels ;
+	Uint32 * dest_pixels = neg_surface -> pixels;
+	
+	for(int i=0; i < neg_surface -> h; i++)
+		for(int j=0; j < neg_surface ->w; j++){
 			SDL_Color c={0};
-			SDL_GetRGBA(pixel_tmp[i * img->w + j], neg_img->format, &c.r, &c.g, &c.b, &c.a);
-			Uint32 c_neg=SDL_MapRGBA(neg_img->format, 255 - c.r, 255 - c.g, 255 - c.b, c.a);
-			pixel_neg_tmp[i * img->w + j]=c_neg;
+			SDL_GetRGBA(src_pixels[i * surface->w + j], neg_surface -> format, &c.r, &c.g, &c.b, &c.a);
+			Uint32 c_neg = SDL_MapRGBA(neg_surface->format, 255 - c.r, 255 - c.g, 255 - c.b, c.a);
+			dest_pixels[i * img->w + j]=c_neg;
 		}
 
-	SDL_UnlockSurface(neg_img);
-	SDL_UnlockSurface(img);
-	SDL_Surface *tmp=img;
-	img=neg_img;
+	SDL_UnlockSurface(neg_surface);
+	SDL_UnlockSurface(surface);
+	SDL_Surface *tmp=surface;
+	set_img_surface(img, neg_surface);
 	SDL_FreeSurface(tmp);
 	return 1;
 }
@@ -45,45 +47,45 @@ short negative_filter(SDL_Surface *img){
 /**
  * Change all surface pixels with black and white filter
  *
- * @param SDL_surface * img , pointer to a surface representing an image
+ * @param image * img , pointer to an image structure representing an image
  * @return 0 if changes failed , 1 if all changes done.
  */
 
-short black_and_white_filter(SDL_Surface *img){
+short black_and_white_filter(image *img){
 	if(img == NULL){
 		fprintf(stderr, "Error black_and_white_filter() : Null argument \n");
 		return 0;
 	}
-
-	SDL_Surface *bnw_img=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
-	if(bnw_img == NULL){
-			fprintf(stderr,"SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
-			return 0;
+	SDL_Surface * surface = img->img;
+	SDL_Surface *bnw_surface=SDL_CreateRGBSurfaceWithFormat(0, surface->h, surface->w, 32, surface->format->format);
+	if(bnw_surface == NULL){
+		fprintf(stderr, "SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+		return 0;
 	}
 
-	if(SDL_MUSTLOCK(bnw_img) == 1 )SDL_LockSurface(bnw_img);
-	if(SDL_MUSTLOCK(img) == 1 )SDL_LockSurface(img);
-	
-	Uint32 *pixel_tmp=img->pixels;
-	Uint32 *bnw_pixel_tmp=bnw_img->pixels;
+	if(SDL_MUSTLOCK(bnw_surface) == 1) SDL_LockSurface(bnw_surface);
+	if(SDL_MUSTLOCK(surface) == 1) SDL_LockSurface(surface);
 
-	for(int i=0; i < bnw_img->h; i++)
-		for(int j=0; j < bnw_img->w; j++){
+	Uint32 * src_pixels = surface -> pixels ;
+	Uint32 * dest_pixels = bnw_surface -> pixels;
+
+	for(int i=0; i < bnw_surface->h; i++)
+		for(int j=0; j < bnw_surface->w; j++){
 			SDL_Color c={0};
-			SDL_GetRGBA(pixel_tmp[i * img->w + j], bnw_img->format, &c.r, &c.g, &c.b, &c.a);
-			int    gray_scale=(int)c.r * 0.2125 + c.g * 0.7154 + c.b * 0.0721;   	 /*CIE 709 recommandation for grayscale*/
+			SDL_GetRGBA(src_pixels[i * img->w + j], bnw_surface->format, &c.r, &c.g, &c.b, &c.a);
+			int    gray_scale=(int)c.r * 0.2125 + c.g * 0.7154 + c.b * 0.0721;       /*CIE 709 recommandation for grayscale*/
 			Uint32 c_bnw;
 			if(gray_scale < 128)
-				c_bnw=SDL_MapRGBA(bnw_img->format, 0, 0, 0, c.a);
+				c_bnw=SDL_MapRGBA(bnw_surface->format, 0, 0, 0, c.a);
 			else
-				c_bnw=SDL_MapRGBA(bnw_img->format, 255, 255, 255, c.a);
-			bnw_pixel_tmp[i * bnw_img->w + j]=c_bnw;
+				c_bnw=SDL_MapRGBA(bnw_surface->format, 255, 255, 255, c.a);
+			dest_pixels[i * bnw_surface->w + j]=c_bnw;
 		}
 
-	SDL_UnlockSurface(bnw_img);
-	SDL_UnlockSurface(img);
-	SDL_Surface *tmp=img;
-	img=bnw_img;
+	SDL_UnlockSurface(bnw_surface);
+	SDL_UnlockSurface(surface);
+	SDL_Surface *tmp=surface;
+	set_img_surface(img,bnw_surface);
 	SDL_FreeSurface(tmp);
 	return 1;
 }
@@ -91,55 +93,56 @@ short black_and_white_filter(SDL_Surface *img){
 /**
  * Change all surface pixels with grey filter
  *
- * @param SDL_surface * img , pointer to a surface representing an image
+ * @param image * img , pointer to image structure representing an image
  * @return 0 if changes failed , 1 if all changes done.
  */
 
-short grey_filter(SDL_Surface *img){
+short grey_filter(image *img){
 	if(img == NULL){
 		fprintf(stderr, "Error grey_filter() : Null argument \n");
 		return 0;
 	}
 
-	SDL_Surface *gray_img=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
-	if(gray_img == NULL){
-			fprintf(stderr,"SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
-			return 0;
+	SDL_Surface * surface = img -> img;
+	SDL_Surface *gray_surface=SDL_CreateRGBSurfaceWithFormat(0, surface->h, surface->w, 32, surface->format->format);
+	if(gray_surface == NULL){
+		fprintf(stderr, "SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+		return 0;
 	}
-	
-	if(SDL_MUSTLOCK(gray_img) == 1) SDL_LockSurface(gray_img);
-	if(SDL_MUSTLOCK(img) == 1) SDL_LockSurface(img);
-	
-	Uint32 *pixel_tmp=img->pixels;
-	Uint32 *gray_pixel_tmp=img->pixels;
 
-	for(int i=0; i < gray_img->h; i++)
-		for(int j=0; j < gray_img->w; j++){
+	if(SDL_MUSTLOCK(gray_surface) == 1) SDL_LockSurface(gray_surface);
+	if(SDL_MUSTLOCK(surface) == 1) SDL_LockSurface(surface);
+	
+	Uint32 * src_pixels = surface -> pixels ;
+	Uint32 * dest_pixels = gray_surface -> pixels;
+	
+	for(int i=0; i < gray_surface->h; i++)
+		for(int j=0; j < gray_surface->w; j++){
 			SDL_Color c={0};
-			SDL_GetRGBA(pixel_tmp[i * img->w + j], gray_img->format, &c.r, &c.g, &c.b, &c.a);
+			SDL_GetRGBA(src_pixels[i * surface->w + j], gray_surface ->format, &c.r, &c.g, &c.b, &c.a);
 			int    gray_scale=(int)((c.r * 0.2125) + (c.g * 0.7154) + (c.b * 0.0721));      /*CIE 709 recommandation for grayscale*/
-			Uint32 c_gray=SDL_MapRGBA(gray_img->format, gray_scale, gray_scale, gray_scale, c.a);
-			gray_pixel_tmp[i * gray_img->w + j]=c_gray;
+			Uint32 c_gray=SDL_MapRGBA(gray_surface->format, gray_scale, gray_scale, gray_scale, c.a);
+			dest_pixels[i * gray_surface->w + j]=c_gray;
 		}
 
-	SDL_UnlockSurface(gray_img);
-	SDL_UnlockSurface(img);
-	SDL_Surface *tmp=img;
-	img=gray_img;
+	SDL_UnlockSurface(gray_surface);
+	SDL_UnlockSurface(surface);
+	SDL_Surface *tmp=surface;
+	set_img_surface(img,gray_surface);
 	SDL_FreeSurface(tmp);
 	return 1;
 }
 
 /**
  * calculates the percentage of distance between two colors
- * @param SDL_Color current_color , color to compare 
+ * @param SDL_Color current_color , color to compare
  * @param SDL_Color origin_color , color to compare
  * @param int margin , margin percentage
  * @return short , 0 if the distance between the two colors is bigger than the percentage , 1 if the distance is acceptable
  */
 
 static short margin_colors(SDL_Color current_color, SDL_Color origin_color, int margin){
-	if(margin == 0 ) return 1;
+	if(margin == 0) return 1;
 	double delta_r=current_color.r - origin_color.r;
 	double delta_g=current_color.g - origin_color.g;
 	double delta_b=current_color.b - origin_color.b;
@@ -153,56 +156,51 @@ static short margin_colors(SDL_Color current_color, SDL_Color origin_color, int 
 }
 
 /**
- * Change surface pixels color with an another (neighboring) color 
+ * Change surface pixels color with an another (neighboring) color
  *
- * @param SDL_surface * img , pointer to a surface representing an image
- * @param SDL_Color origin , represent the color to replace 
+ * @param image * img , pointer to image structure representing an image
+ * @param SDL_Color origin , represent the color to replace
  * @param SDL_Color target_color , represent the color to apply
  * @param int margin , represent the percentage of color proximity level
  * @return 0 if changes failed , 1 if all changes done.
  */
 
-short replace_color(SDL_Surface *img, SDL_Color origin_color, SDL_Color target_color, int margin){
+short replace_color(image *img, SDL_Color origin_color, SDL_Color target_color, int margin){
 	if(img == NULL){
 		fprintf(stderr, "Error replace_color() : Null argument \n");
 		return 0;
 	}
-
-	SDL_Surface *repl_img=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
-	if(repl_img == NULL){
-			fprintf(stderr,"SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
-			return 0;
+	SDL_Surface * surface = img -> img;
+	SDL_Surface *repl_surface = SDL_CreateRGBSurfaceWithFormat(0, surface->h, surface->w, 32, surface->format->format);
+	if(repl_surface == NULL){
+		fprintf(stderr, "SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+		return 0;
 	}
 
-	if(SDL_MUSTLOCK(repl_img) == SDL_TRUE) SDL_LockSurface(repl_img);
-	if(SDL_MUSTLOCK(img) == SDL_TRUE) SDL_LockSurface(img);
-	
-	Uint32 *pixel_tmp=img->pixels;
-	Uint32 *repl_pixel_tmp=img->pixels;
+	if(SDL_MUSTLOCK(repl_surface) == SDL_TRUE) SDL_LockSurface(repl_surface);
+	if(SDL_MUSTLOCK(surface) == SDL_TRUE) SDL_LockSurface(surface);
 
-	for(int i=0; i < repl_img->h; i++)
-		for(int j=0; j < repl_img->w; j++){
-			SDL_Color current_color={0};
-			SDL_GetRGBA(pixel_tmp[i * img->w + j], repl_img->format, &current_color.r, &current_color.g, &current_color.b, &current_color.a);
+	Uint32 * src_pixels = surface -> pixels ;
+	Uint32 * dest_pixels = repl_surface -> pixels;
+
+	for(int i=0; i < repl_surface->h; i++)
+		for(int j=0; j < repl_surface->w; j++){
+			SDL_Color c={0};
+			SDL_GetRGBA(src_pixels[i * surface->w + j], repl_surface->format, &c.r, &c.g, &c.b, &c.a);
 			Uint32 repl_c;
-			if(margin_colors(current_color, origin_color, margin) == 1)
-				repl_c=SDL_MapRGBA(repl_img->format, target_color.r, target_color.g, target_color.b, target_color.a);
+			if(margin_colors(c, origin_color, margin) == 1)
+				repl_c=SDL_MapRGBA(repl_surface ->format, target_color.r, target_color.g, target_color.b, target_color.a);
 			else
-				repl_c=pixel_tmp[i * img->w + j];
-			repl_pixel_tmp[i * repl_img->w + j]=repl_c;
+				repl_c=src_pixels[i * surface->w + j];
+			dest_pixels[i * repl_surface->w + j]=repl_c;
 		}
 
-	SDL_UnlockSurface(repl_img);
-	SDL_UnlockSurface(img);
-	SDL_Surface *tmp=img;
-	img=repl_img;
+	SDL_UnlockSurface(repl_surface);
+	SDL_UnlockSurface(surface);
+	SDL_Surface *tmp=surface;
+	set_img_surface(img, repl_surface);
 	SDL_FreeSurface(tmp);
 	return 1;
-}
-
-
-static int max(int x, int y){
-	return x > y ? x : y;
 }
 
 static int min(int x, int y){
@@ -210,46 +208,40 @@ static int min(int x, int y){
 }
 
 /**
- * Change all surface pixels with negative filter
- * 
- * @param SDL_surface * img , pointer to a surface representing an image
- * @param SDL_color , represents the color to apply in the chosen area 
+ * Fill rectangle on image surface 
+ *
+ * @param img * img , pointer to image structure representing an image
+ * @param SDL_color , represents the color to apply in the chosen area
  * @param int x1 , x2 , y1 , y2 , represent the area coordinates on the target surface
  * @return 0 if changes failed , 1 if all changes done.
  */
-short color_zone(SDL_Surface *img, SDL_Color color, int x1, int x2, int y1, int y2){
-	if(img == NULL ){
+short color_zone(image *img, SDL_Color color, int x1, int x2, int y1, int y2){
+	if(img == NULL){
 		fprintf(stderr, "Error color_zone() : Null argument\n");
 		return 0;
 	}
-
-	SDL_Surface *zone_img=SDL_CreateRGBSurfaceWithFormat(0, img->h, img->w, 32, img->format->format);
-	if(zone_img == NULL){
-			fprintf(stderr,"SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
-			return 0;
+	SDL_Surface * surface = img -> img;
+	SDL_Surface *zone_surface=SDL_CreateRGBSurfaceWithFormat(0, surface->h, surface ->w, 32, surface->format->format);
+	if(zone_surface == NULL){
+		fprintf(stderr, "SDL_CreateRGBSurfaceWithFormat() failed: %s", SDL_GetError());
+		return 0;
+	}
+	if(SDL_BlitSurface(surface,NULL,zone_surface,NULL) != 0){
+		fprintf(stderr, "SDL_BlitSurface() failed: %s", SDL_GetError());
+		return 0;
 	}
 
-	if(SDL_MUSTLOCK(zone_img) == 1)SDL_LockSurface(zone_img);
-	if(SDL_MUSTLOCK(img) == 1 ) SDL_LockSurface(img);
-	
-	Uint32 *pixel_tmp=img->pixels;
-	Uint32 *zone_pixel_tmp=img->pixels;
+	if(SDL_MUSTLOCK(zone_surface) == 1) SDL_LockSurface(zone_surface);
+	/*if(SDL_MUSTLOCK(img) == 1) SDL_LockSurface(surface);*/
 
-	for(int i=0; i < zone_img->h; i++)
-		for(int j=0; j < zone_img->w; j++){
-			SDL_Color color_c={0};
-			Uint32    c_zone;
-			if(i >= min(y1, y2) && i <= max(y1, y2) && j >= min(x1, x2) && j <= max(x1, x2))
-				c_zone=SDL_MapRGBA(zone_img->format, color_c.r, color_c.g, color_c.b, color_c.a);
-			else
-				c_zone=pixel_tmp[i * img->w + j];
-			zone_pixel_tmp[i * zone_img->w + j]=c_zone;
-		}
+	SDL_Rect rect = {min(x1,x2),min(y1,y2),abs(x2-x1),abs(y2-y1)};
+	Uint32 r_color = SDL_MapRGBA (zone_surface -> format , color.r , color.g , color.b , color.a );
+	SDL_FillRect (zone_surface , &rect , r_color);
 
-	SDL_UnlockSurface(zone_img);
-	SDL_UnlockSurface(img);
-	SDL_Surface *tmp=img;
-	img=zone_img;
+	SDL_UnlockSurface(zone_surface);
+	/*SDL_UnlockSurface(surface);*/
+	SDL_Surface *tmp=surface;
+	set_img_surface(img,zone_surface);
 	SDL_FreeSurface(tmp);
 	return 1;
 }

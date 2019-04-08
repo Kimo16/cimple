513ddff0eddcc879e53c27f776913c5c56f088fc
leethater <budzkoal@gmail.com>
2019-04-08 14:00:31 +0200

    changes

--- src/model/m_frame.c
+++ src/model/m_frame.c
@@ -1,11 +1,34 @@
 #include "m_frame.h"
 
+static int get_int_max(int a, int b){
+	if(a>b) return a;
+	return b;
+}
+
+static int get_int_min(int a, int b){
+	if(a<b) return a;
+	return b;
+}
+
 short truncate_image(SDL_Surface *img, int x1, int y1, int x2, int y2){
-	return 0;
+	// check if image is null
+	if(img == NULL){
+		fprintf(stderr, "Null image in truncate");
+		return 0;
+	}
+	// checking if coords are in the box
+	if(get_int_min(x1,x2)<0 || get_int_min(y1,y2)<0 || get_int_max(x1,x2)>surface->w || get_int_max(y1,y2)>surface->h){
+		fprintf(stderr, "Wrong coordinated in truncate");
+		return 0;
+	}
 }
 
 short resize_workspace(SDL_Surface *img, int witdh, int height){
-	return 0;
+	if(img == NULL){
+		fprintf(stderr, "Null image in truncate");
+		return 0;
+	}
+	return 1;
 }
 
 short resize_image(SDL_Surface *img, int width, int height){

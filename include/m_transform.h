16e4c99aedaff7d92bfca777e1f35a099e1c1f74
Kolibs <etienne@marais.green>
2019-04-09 09:21:53 +0200

    Add copy / cut / paste

--- include/m_transform.h
+++ include/m_transform.h
@@ -6,6 +6,8 @@
 
 short symmetry(image * target,short vertical);
 short rotate(image * target, int angle);
-
+short copy(image *, SDL_Rect);
+short cut(image *, SDL_Rect);
+short past(image *, int x, int y);
 
 #endif
--- src/model/m_transform.c
+++ src/model/m_transform.c
@@ -1,5 +1,9 @@
 #include "m_transform.h"
 
+static Uint32 * buffer = NULL;
+static int buffer_h = 0;
+static int buffer_w = 0;
+
 /**
  *
  * Colors the (x,y) pixel with the specific color
@@ -188,4 +192,134 @@ short rotate(image *target, int angle){
 		return 0;
 	}
 	return 1;
+}
+
+
+/**
+ * @brief
+ * Free the buffer that stores copy
+ */
+static void free_buffer() {
+  if(buffer != NULL) {
+    Uint32 * tmp = buffer;
+    buffer = NULL;
+    free(tmp);
+  }
+}
+
+/**
+ * @brief
+ * Tell if the pixel is inside the image
+ */
+static short is_in_image(int x, int y, int w, int h) {
+ return (0 < y) && (y < h) && (0 < x) && (x < w); 
+}
+
+/*
+ * @brief
+ * Copy an area into the buffer
+ *
+ * @param img image on which you have to copy
+ * @param SDL_Rect area where you want to copy
+ * @return 0 in case of error 1 else
+ */
+short copy(image *img, SDL_Rect area) {
+  if(img == NULL){
+		fprintf(stderr, "Null image in copy\n");
+		return 0;
+	}
+	SDL_Surface *current = get_img_surface(img);
+	if(current == NULL){
+		fprintf(stderr, "Null surface in copy\n");
+		return 0;
+	}
+  free_buffer();
+  buffer_h = area.h;
+  buffer_w = area.w;
+  buffer = malloc(area.w * area.h * sizeof(Uint32));
+  if (buffer == NULL) {
+    fprintf(stderr, "Buffer copy error\n");
+    return 0;
+  }
+  if(SDL_MUSTLOCK(current) == SDL_TRUE) SDL_LockSurface(current);
+	Uint32 *pixels = current->pixels;
+	
+  for(int i = 0 ; i < area.h ; i++) {
+    for (int j = 0 ; j < area.w ; j++) {
+      buffer[i*area.w+j] = pixels[(area.y+i)*area.w + (area.x+j)]; 
+    }
+  }
+  SDL_UnlockSurface(current);
+  return 1;
+}
+
+/*
+ * @brief
+ * Cut an area into the buffer
+ *
+ * @param img image on which you have to copy
+ * @param SDL_Rect area where you want to copy
+ * @return 0 in case of error 1 else
+ */
+short cut(image *img, SDL_Rect area) {
+  if(img == NULL){
+		fprintf(stderr, "Null image in cut\n");
+		return 0;
+	}
+	SDL_Surface *current = get_img_surface(img);
+	if(current == NULL){
+		fprintf(stderr, "Null surface in cut\n");
+		return 0;
+	}
+  free_buffer();
+  
+  buffer_h = area.h; 
+  buffer_w = area.w;
+  buffer = malloc(area.w * area.h * sizeof(Uint32));
+  if (buffer == NULL) {
+    fprintf(stderr, "Buffer cut error\n");
+    return 0;
+  }
+  if(SDL_MUSTLOCK(current) == SDL_TRUE) SDL_LockSurface(current);
+	Uint32 *pixels = current->pixels;
+	
+  for(int i = 0 ; i < area.h ; i++) {
+    for (int j = 0 ; j < area.w ; j++) {
+      buffer[i*area.w+j] = pixels[(area.y+i)*area.w + (area.x+j)]; 
+      pixels[(area.y+i)*area.w + (area.x+j)] = 0; // Couleur noire 
+    }
+  }
+  SDL_UnlockSurface(current);
+  return 1;
+}
+
+
+/**
+ * @brief
+ * Paste buffer onto img->surface
+ * @param img image to paste onto
+ * @param x y coordinates
+ */
+short paste(image *img, int x, int y) {
+  if (buffer == NULL) return 1;
+   if(img == NULL){
+		fprintf(stderr, "Null image in paste\n");
+		return 0;
+	}
+	SDL_Surface *current = get_img_surface(img);
+	if(current == NULL){
+		fprintf(stderr, "Null surface in paste\n");
+		return 0;
+	}
+  if(SDL_MUSTLOCK(current) == SDL_TRUE) SDL_LockSurface(current);
+	Uint32 *pixels = current->pixels;
+  for(int i = 0 ; i < buffer_h ; i++) {
+    for (int j = 0 ; j < buffer_w ; j++) {
+      if (is_in_image(x+j, y+i ,current->w,current->h)) {
+          pixels[(y+i)*buffer_w + (x+j)] = buffer[i*buffer_w+j];
+      }
+    }
+  }
+  SDL_UnlockSurface(current);
+  return 1;
 }

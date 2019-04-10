da19c10c92681f28d7fe86805448f8d4b6a735c6
Kolibs <etienne@marais.green>
2019-04-10 13:42:49 +0200

    Correct typos

--- include/m_transform.h
+++ include/m_transform.h
@@ -8,6 +8,6 @@ short symmetry(image * target,short vertical);
 short rotate(image * target, int angle);
 short copy(image *, SDL_Rect);
 short cut(image *, SDL_Rect);
-short past(image *, int x, int y);
+short paste(image *, int x, int y);
 
 #endif

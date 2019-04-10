57ba7cdb8e62fe16137f510b40c3ba173a1b80fe
Kimo16 <karimmezher@hotmail.fr>
2019-04-10 14:52:56 +0200

    add header function and comment light_filter()

--- include/m_color.h
+++ include/m_color.h
@@ -8,5 +8,5 @@ short black_and_white_filter(image * img, SDL_Rect rect);
 short grey_filter(image * img,SDL_Rect rect );
 short replace_color(image * img, SDL_Rect rect , SDL_Color origin_color,SDL_Color target_color,int margin);
 short color_zone(image * img ,SDL_Rect rect , SDL_Color color);
-
+short light_filter(image * img , SDL_Rect rect , int percent );
 #endif

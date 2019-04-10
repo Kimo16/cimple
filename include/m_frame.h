fc2a2d0cd0f1d44be394b39b6c8c72a14dcc9641
leethater <budzkoal@gmail.com>
2019-04-10 14:05:56 +0200

    resize and comments

--- include/m_frame.h
+++ include/m_frame.h
@@ -3,6 +3,7 @@
 
 #include <SDL2/SDL.h>
 #include "m_image.h"
+#include "resize.h"
 
 	short truncate_image(image * target , int x1 , int y1 , int x2 ,int y2);
 	short resize_workspace(image * target,int witdh , int height );
--- src/model/m_frame.c
+++ src/model/m_frame.c
@@ -1,14 +1,44 @@
 #include "m_frame.h"
 
+/**
+ * @brief 
+ * Returns the maximum of two numbers
+ *
+ * @param a first number
+ * @param b second number
+ * @return maximum of two integers
+ */
+
 static int get_int_max(int a, int b){
 	if(a > b) return a;
 	return b;
 }
 
+/**
+ * @brief 
+ * Returns the minimum of two numbers
+ *
+ * @param a first number
+ * @param b second number
+ * @return minimum of two integers
+ */
+
 static int get_int_min(int a, int b){
 	if(a < b) return a;
 	return b;
 }
+
+/**
+ * @brief 
+ * Truncates the image
+ *
+ * @param target image 
+ * @param x1 x coordinate of the first summit
+ * @param y1 y coordinate of the first summit
+ * @param x2 x coordinate of the second summit
+ * @param y2 y coordinate of the second summit
+ * @return 1 if succeded, 0 if failed
+ */
 
 short truncate_image(image *target, int x1, int y1, int x2, int y2){
 	if(target == NULL){
@@ -54,7 +84,17 @@ short truncate_image(image *target, int x1, int y1, int x2, int y2){
 	return 1;
 }
 
-short resize_workspace(image * target, int width_p, int height_p){
+/**
+ * @brief 
+ * Resizes the workspace, width and height may be negative
+ *
+ * @param target image 
+ * @param width_p the value added to the width
+ * @param height_p the value added to the height
+ * @return 1 if succeded, 0 if failed
+ */
+
+short resize_workspace(image *target, int width_p, int height_p){
 	if(target == NULL){
 		fprintf(stderr, "Null image in truncate");
 		return 0;
@@ -64,8 +104,8 @@ short resize_workspace(image * target, int width_p, int height_p){
 		fprintf(stderr, "Null surface in truncate");
 		return 0;
 	}
-	int width_new = surface->w+width_p;
-	int height_new = surface->h+height_p;
+	int          width_new=surface->w + width_p;
+	int          height_new=surface->h + height_p;
 	SDL_Surface *new_surface;
 	new_surface=SDL_CreateRGBSurfaceWithFormat(0, width_new, height_new, 32, surface->format->format);
 	if(SDL_MUSTLOCK(new_surface) == SDL_TRUE) SDL_LockSurface(new_surface);
@@ -75,14 +115,13 @@ short resize_workspace(image * target, int width_p, int height_p){
 	for(int i=0; i < height_new; i++)
 		for(int j=0; j < width_new; j++){
 			Uint32 new_color;
-			if(i > surface->h || j > surface->w){
+			if(i > surface->h || j > surface->w)
 				new_color=SDL_MapRGBA(surface->format, 0, 0, 0, 255);
-			}
 			else{
 				SDL_Color c_ref={0};
 				SDL_GetRGBA(pixels_ref[i * surface->w + j], new_surface->format, &c_ref.r, &c_ref.g, &c_ref.b, &c_ref.a);
 				new_color=SDL_MapRGBA(surface->format, c_ref.r, c_ref.g, c_ref.b, c_ref.a);
-				}
+			}
 			pixels_new[i * width_new + j]=new_color;
 		}
 	SDL_UnlockSurface(new_surface);
@@ -94,6 +133,37 @@ short resize_workspace(image * target, int width_p, int height_p){
 	return 1;
 }
 
-short resize_image(image * target, int width, int height){
-	return 0;
+/**
+ * @brief 
+ * Resizes the image
+ *
+ * @param target image 
+ * @param width new width
+ * @param height new height
+ * @return 1 if succeded, 0 if failed
+ */
+
+short resize_image(image *target, int width, int height){
+	if(target == NULL){
+		fprintf(stderr, "Null image in truncate");
+		return 0;
+	}
+	SDL_Surface *surface=get_img_surface(target);
+	if(surface == NULL){
+		fprintf(stderr, "Null surface in truncate");
+		return 0;
+	}
+	if(SDL_MUSTLOCK(surface) == SDL_TRUE) SDL_LockSurface(surface);
+	SDL_Surface *new_surface = SDL_ResizeXY(surface, width, height, 6);
+	if(new_surface==NULL){
+		fprintf(stderr, "Resize failed");
+		return 0;
+	}
+	SDL_UnlockSurface(new_surface);
+	SDL_UnlockSurface(surface);
+	if(set_img_surface(target, new_surface) == 0){
+		fprintf(stderr, "Surface not set");
+		return 0;
+	}
+	return 1;
 }

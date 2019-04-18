d9fc0b0ad6c037b99de2ad6429e16064c4c449b8
leethater <budzkoal@gmail.com>
2019-04-18 16:44:45 +0200

    rotate patch, added reverse

--- include/m_transform.h
+++ include/m_transform.h
@@ -5,7 +5,7 @@
 #include "m_image.h"
 
 short symmetry(image * target,short vertical);
-short rotate(image * target, int angle);
+short rotate(image * target, int angle, short reverse);
 short copy(image *, SDL_Rect);
 short cut(image *, SDL_Rect);
 short paste(image *, int x, int y);

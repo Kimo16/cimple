e9b8e6feb0223815e4cd1d02364516c6ebb428c7
leethater <budzkoal@gmail.com>
2019-05-02 16:04:43 +0200

    workspace and image resize fix

--- src/view/window.c
+++ src/view/window.c
@@ -3,45 +3,45 @@
 frame *init_frame(char *path){
 	frame *new_frame=malloc(sizeof(frame));
 	if(new_frame == NULL){
-		fprintf(stderr, "Frame not initialized");
+		fprintf(stderr, "Frame not initialized\n");
 		return NULL;
 	}
 	new_frame->image=load_image(path);
 	if(new_frame->image == NULL){
-		fprintf(stderr, "Image could not be set");
+		fprintf(stderr, "Image could not be set\n");
 		free_frame(new_frame);
 		return NULL;
 	}
 	SDL_Surface *surface=get_img_surface(new_frame->image);
 	if(surface == NULL){
-		fprintf(stderr, "Cannot get surface");
+		fprintf(stderr, "Cannot get surface\n");
 		free_frame(new_frame);
 		return NULL;
 	}
 	new_frame->window=SDL_CreateWindow(get_img_name(new_frame->image), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, surface->w, surface->h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	if(new_frame->window == NULL){
-		fprintf(stderr, "Cannot create window");
+		fprintf(stderr, "Cannot create window\n");
 		free_frame(new_frame);
 		return NULL;
 	}
-	new_frame->renderer=SDL_CreateRenderer(new_frame->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);  // trying to use hardware acceleration for the renderer
+	new_frame->renderer=SDL_CreateRenderer(new_frame->window, -1, SDL_RENDERER_ACCELERATED);  // trying to use hardware acceleration for the renderer
 	if(new_frame->renderer == NULL){
-		new_frame->renderer=SDL_CreateRenderer(new_frame->window, -1, SDL_RENDERER_SOFTWARE | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE); // the renderer is a software fallback
+		new_frame->renderer=SDL_CreateRenderer(new_frame->window, -1, SDL_RENDERER_SOFTWARE); // the renderer is a software fallback
 		if(new_frame->renderer == NULL){
-			fprintf(stderr, "Renderer not initialized");
+			fprintf(stderr, "Renderer not initialized\n");
 			free_frame(new_frame);
 			return NULL;
 		}
 	}
 	SDL_Texture *texture=SDL_CreateTextureFromSurface(new_frame->renderer, surface);
 	if(texture == NULL){
-		fprintf(stderr, "Error SDL_CreateTextureFromSurface : %s", SDL_GetError());
+		fprintf(stderr, "Error SDL_CreateTextureFromSurface : %s\n", SDL_GetError());
 		free_frame(new_frame);
 		return NULL;
 	}
 	int rc=SDL_RenderCopy(new_frame->renderer, texture, NULL, NULL);
 	if(rc < 0){
-		fprintf(stderr, "Render error");
+		fprintf(stderr, "Render error\n");
 		SDL_DestroyTexture(texture);
 		free_frame(new_frame);
 		return NULL;
@@ -57,7 +57,7 @@ short update_frame(frame *target, char *path){
 		image *img=target->image;
 		target->image=load_image(path);
 		if(target->image == NULL){
-			fprintf(stderr, "Image could not be set");
+			fprintf(stderr, "Image could not be set\n");
 			target->image=img;
 			return 0;
 		}
@@ -66,21 +66,29 @@ short update_frame(frame *target, char *path){
 	SDL_RenderClear(target->renderer);
 	SDL_Surface *surface=get_img_surface(target->image);
 	if(surface == NULL){
-		fprintf(stderr, "Error getting surface");
+		fprintf(stderr, "Error getting surface\n");
 		free_frame(target);
 		return 0;
 	}
 	SDL_SetWindowSize(target->window, surface->w, surface->h);
+	SDL_DestroyRenderer(target->renderer);
+	target->renderer=SDL_CreateRenderer(target->window, -1, SDL_RENDERER_ACCELERATED);  // trying to use hardware acceleration for the renderer
+	if(target->renderer == NULL){
+		target->renderer=SDL_CreateRenderer(target->window, -1, SDL_RENDERER_SOFTWARE); // the renderer is a software fallback
+		if(target->renderer == NULL){
+			fprintf(stderr, "Renderer could not be reinitialized\n");
+			return 0;
+		}
+	}
 	SDL_Texture *new_texture=SDL_CreateTextureFromSurface(target->renderer, surface);
 	if(new_texture == NULL){
-		fprintf(stderr, "Error updating texture");
+		fprintf(stderr, "Error updating texture\n");
 		free_frame(target);
 		return 0;
 	}
-	SDL_RenderPresent(target->renderer);
 	int rc=SDL_RenderCopy(target->renderer, new_texture, NULL, NULL);
 	if(rc < 0){
-		fprintf(stderr, "Render error");
+		fprintf(stderr, "Render error\n");
 		SDL_DestroyTexture(new_texture);
 		free_frame(target);
 		return 0;

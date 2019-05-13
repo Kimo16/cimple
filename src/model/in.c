8af7abcff60b83bd3e6b89a673d23a88e4a3e69a
Kolibs <etienne@marais.green>
2019-05-13 16:52:04 +0200

    Uncrustify

--- src/model/in.c
+++ src/model/in.c
@@ -5,27 +5,27 @@
  * [path]
  */
 image *load_image(char *path){
-	image *img=new_img(path);
-	if(img == NULL){
+	image *img = new_img(path);
+	if (img == NULL) {
 		fprintf(stderr, "DEBUG : wrong image path.\n");
 		return NULL;
 	}
-	SDL_Surface *tmp=IMG_Load(path);
-	if(tmp == NULL){
+	SDL_Surface *tmp = IMG_Load(path);
+	if (tmp == NULL) {
 		fprintf(stderr, "DEBUG : can't load image.\n");
 		free_image(img);
 		return NULL;
 	}
-	SDL_PixelFormat *format=SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
-	if(format == NULL){
+	SDL_PixelFormat *format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
+	if (format == NULL) {
 		fprintf(stderr, "DEBUG : can't allocate format.\n");
 		SDL_FreeSurface(tmp);
 		free_image(img);
 		return NULL;
 	}
-	SDL_Surface *copy=SDL_ConvertSurface(tmp, format, 0);
+	SDL_Surface *copy = SDL_ConvertSurface(tmp, format, 0);
 	SDL_FreeSurface(tmp);
-	if(copy == NULL){
+	if (copy == NULL) {
 		fprintf(stderr, "DEBUG : can't convert image.\n");
 		SDL_FreeFormat(format);
 		return NULL;
@@ -36,19 +36,19 @@ image *load_image(char *path){
 }
 
 short check_tmp(){
-	char *tmp_dir="/tmp/cimpletmp/";
-	DIR * dir=opendir(tmp_dir);
-	int   ret=0;
-	if(dir == NULL){
+	char *tmp_dir = "/tmp/cimpletmp/";
+	DIR * dir = opendir(tmp_dir);
+	int   ret = 0;
+	if (dir == NULL) {
 		fprintf(stderr, "No file saved");
 		return ret;
 	}
 	struct dirent *current;
-	while((current=readdir(dir)) != NULL){
-		if(memcmp(current->d_name, "./", 2) == 0 ||
-		   memcmp(current->d_name, "../", 3) == 0)
+	while ((current = readdir(dir)) != NULL) {
+		if (memcmp(current->d_name, "./", 2) == 0 ||
+		    memcmp(current->d_name, "../", 3) == 0)
 			continue;
-		ret+=1;
+		ret += 1;
 		printf("%s%s", tmp_dir, current->d_name);
 	}
 	closedir(dir);
--- src/model/out.c
+++ src/model/out.c
@@ -10,16 +10,16 @@
  * @param jpg the jpg structure in which writes
  */
 static void export_pixels_jpg(SDL_Surface *surface, unsigned char *row, j_compress_ptr jpg){
-	uint32_t *pixels=surface->pixels;
-	SDL_Color c={0};
+	uint32_t *pixels = surface->pixels;
+	SDL_Color c = {0};
 	int       i, j;
 	SDL_LockSurface(surface);
-	for(i=0; i < surface->h; i++){
-		for(j=0; j < surface->w; j++){
+	for (i = 0; i < surface->h; i++) {
+		for (j = 0; j < surface->w; j++) {
 			SDL_GetRGBA(pixels[i * surface->w + j], surface->format, &c.r, &c.g, &c.b, &c.a);
-			row[j * 3]=c.r;
-			row[j * 3 + 1]=c.g;
-			row[j * 3 + 2]=c.b;
+			row[j * 3] = c.r;
+			row[j * 3 + 1] = c.g;
+			row[j * 3 + 2] = c.b;
 		}
 		jpeg_write_scanlines(jpg, &row, 1);
 	}
@@ -45,9 +45,9 @@ static short save_bmp(image *img){
  * @param img the image with a surface to save
  */
 static short save_png(image *img){
-	SDL_Surface *surface=get_img_surface(img);
-	char *       file=get_full_image_path(img);
-	if(!IMG_SavePNG(surface, file))
+	SDL_Surface *surface = get_img_surface(img);
+	char *       file = get_full_image_path(img);
+	if (!IMG_SavePNG(surface, file))
 		return 1;
 	return 0;
 }
@@ -58,32 +58,32 @@ static short save_png(image *img){
  */
 static short save_jpeg(image *img){
 	FILE *       output;
-	SDL_Surface *surface=get_img_surface(img);
-	char *       file=get_full_image_path(img);
+	SDL_Surface *surface = get_img_surface(img);
+	char *       file = get_full_image_path(img);
 	struct jpeg_compress_struct jpg;
 	struct jpeg_error_mgr       jpgerror;
 	unsigned char *row;
 
-	if((output=fopen(file, "wb")) == NULL){
+	if ((output = fopen(file, "wb")) == NULL) {
 		fprintf(stderr, "Can't write image...");
 		return 1;
 	}
 
-	jpg.err=jpeg_std_error(&jpgerror);
+	jpg.err = jpeg_std_error(&jpgerror);
 	jpeg_create_compress(&jpg);
 
 	// Setup image jpeg
 	jpeg_stdio_dest(&jpg, output);
-	jpg.image_width=surface->w;
-	jpg.image_height=surface->h;
-	jpg.input_components=3;
-	jpg.in_color_space=JCS_RGB;
+	jpg.image_width = surface->w;
+	jpg.image_height = surface->h;
+	jpg.input_components = 3;
+	jpg.in_color_space = JCS_RGB;
 	jpeg_set_defaults(&jpg);
 
 
 	// Get format pixels
-	row=malloc(sizeof(unsigned char) * 3 * surface->w);
-	if(row == NULL)
+	row = malloc(sizeof(unsigned char) * 3 * surface->w);
+	if (row == NULL)
 		return 0;
 	else {
 		// Start compression
@@ -93,9 +93,9 @@ static short save_jpeg(image *img){
 	}
 
 	// Free
-	if(!output) fclose(output);
+	if (!output) fclose(output);
 	jpeg_destroy_compress(&jpg);
-	if(!row) free(row);
+	if (!row) free(row);
 	return 1;
 }
 
@@ -108,14 +108,14 @@ static short save_jpeg(image *img){
  * @param format the image format
  */
 image *save_image_as(image *img, char *path){
-	if(img == NULL || path == NULL) return NULL;
-	image *res=new_img(path);
-	if(res == NULL) return NULL;
-	if(!set_img_surface(res, get_img_surface(img))){
+	if (img == NULL || path == NULL) return NULL;
+	image *res = new_img(path);
+	if (res == NULL) return NULL;
+	if (!set_img_surface(res, get_img_surface(img))) {
 		free_image(res);
 		return NULL;
 	}
-	if(!save_image(res))
+	if (!save_image(res))
 		return NULL;
 	return res;
 }
@@ -127,19 +127,19 @@ image *save_image_as(image *img, char *path){
  * @param img the image with all informations
  */
 short save_image(image *img){
-	if(memcmp("jpg", get_img_ext(img), 3) == 0)
+	if (memcmp("jpg", get_img_ext(img), 3) == 0)
 		return save_jpeg(img);
-	else if(memcmp("jpeg", get_img_ext(img), 4) == 0)
+	else if (memcmp("jpeg", get_img_ext(img), 4) == 0)
 		return save_jpeg(img);
-	else if(memcmp("JPEG", get_img_ext(img), 4) == 0)
+	else if (memcmp("JPEG", get_img_ext(img), 4) == 0)
 		return save_jpeg(img);
-	else if(memcmp("JPG", get_img_ext(img), 3) == 0)
+	else if (memcmp("JPG", get_img_ext(img), 3) == 0)
 		return save_jpeg(img);
-	else if(memcmp("png", get_img_ext(img), 3) == 0)
+	else if (memcmp("png", get_img_ext(img), 3) == 0)
 		return save_png(img);
-	else if(memcmp("PNG", get_img_ext(img), 3) == 0)
+	else if (memcmp("PNG", get_img_ext(img), 3) == 0)
 		return save_png(img);
-	else if(memcmp("bmp", get_img_ext(img), 3) == 0)
+	else if (memcmp("bmp", get_img_ext(img), 3) == 0)
 		return save_bmp(img);
 	return 0;
 }
@@ -151,10 +151,10 @@ short save_image(image *img){
  * @param img the image to save
  */
 short save_secure(image *img){
-	char *path="/tmp/cimpletmp/";
-	char *save_name=malloc(strlen(path) + strlen(get_img_name(img)) + 4);
+	char *path = "/tmp/cimpletmp/";
+	char *save_name = malloc(strlen(path) + strlen(get_img_name(img)) + 4);
 	sprintf("%s%s.bmp", path, get_img_name(img));
-	if(save_image_as(img, save_name) == NULL)
+	if (save_image_as(img, save_name) == NULL)
 		return 0;
 	return 1;
 }

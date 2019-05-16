9cba9c39964ed72a5b7980900103bf02c83606cc
Kolibs <etienne@marais.green>
2019-05-16 13:45:21 +0200

    Patch gnome freeze

--- src/main.c
+++ src/main.c
@@ -2,5 +2,14 @@
 #include "cmd_line.h"
 
 int main(){
-	return cimple_handler();
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		return 1;
+	}
+	if(SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_PING,"0") != SDL_TRUE) {
+		// Avoid GNOME Freeze
+		return 1;
+	}
+	int rc = cimple_handler();
+	SDL_Quit();
+	return rc;
 }
